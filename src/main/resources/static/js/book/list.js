f2f106d0944c6e675373cc89e8a44b711359fed2
geon <zqrc05@gmail.com>
2024-08-31 18:42:16 +0900

    페이지네이션 구성중

--- src/main/resources/static/js/book/list.js
+++ src/main/resources/static/js/book/list.js
@@ -1,5 +1,6 @@
 window.addEventListener('load',function(){
   go_insert_book();
+  go_page;
 })
 
 let go_insert_book = function(){
@@ -8,4 +9,8 @@ let go_insert_book = function(){
   btn_insert_book.addEventListener('click',function(){
     location.href='/book/bookInsert'
   })
+}
+
+let go_page = function(index){
+  console.log(index);
 }
